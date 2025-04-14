ce2c9eade9c4c6f276992a3d65763f0f97ad98bd
Ambuj nayan mishra <ambuj6392@gmail.com>
2025-04-15 01:13:44 +0530

    COMMIT

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name= "Hotel-Reservation-Prediction",
+    version="0.1",
+    author="Ambuj Nayan Mishra",
+    packages=find_packages(),
+    install_requires= requirements, 
+)
